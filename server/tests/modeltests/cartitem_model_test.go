3a511cda03a89b89b5b023bb87e29208944cb5bf
Rakesh Mothukuri <rockey5520@gmail.com>
2020-08-08 20:42:38 +0100

    added few more unittests

--- server/tests/modeltests/cartitem_model_test.go
+++ server/tests/modeltests/cartitem_model_test.go
@@ -35,3 +35,37 @@ func TestSaveCartItem(t *testing.T) {
 	assert.Equal(t, savedCartItem.Quantity, newCartItem.Quantity)
 	assert.Equal(t, savedCartItem.ItemTotal, newCartItem.ItemTotal)
 }
+
+func TestUpdateCartItem(t *testing.T) {
+
+	err := refreshCartItemTable()
+	if err != nil {
+		log.Fatalf("Error refreshCartItemTable %v\n", err)
+	}
+
+	_, err = seedOneCartItem()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = seedOneCart()
+	if err != nil {
+		log.Fatal(err)
+	}
+	newCartItem := models.CartItem{
+		CartID:              1,
+		FruitID:             1,
+		Name:                "Apple",
+		Quantity:            1,
+		ItemTotal:           1,
+		ItemDiscountedTotal: 0.0,
+	}
+
+	savedCartItem, err := newCartItem.SaveOrUpdateCartItem(server.DB)
+	if err != nil {
+		t.Errorf("Error while saving a user: %v\n", err)
+		return
+	}
+	assert.Equal(t, savedCartItem.FruitID, newCartItem.FruitID)
+	assert.Equal(t, savedCartItem.Quantity, newCartItem.Quantity)
+	assert.Equal(t, savedCartItem.ItemTotal, newCartItem.ItemTotal)
+}
